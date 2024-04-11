588e90608d6a90fbec6987b64e4cc7ecf086b81d
Trey Stafford <trey.stafford@colorado.edu>
2024-04-11 11:27:47 -0600

    Run `black` formatter on run.py

--- run.py
+++ run.py
@@ -17,26 +17,20 @@ config = Config(
             cores_per_worker=1,
             max_workers_per_node=1,
             worker_logdir_root="/tmp/",
-
             # Address for the pod worker to connect back
             address=address_by_route(),
             provider=KubernetesProvider(
                 namespace="default",
-
                 # Docker image url to use for pods
                 image="python",
-
                 # Command to be run upon pod start, such as:
                 # "module load Anaconda; source activate parsl_env".
                 # or "pip install parsl"
                 worker_init="pip install parsl",
-
                 # The secret key to download the image
                 # secret="YOUR_KUBE_SECRET",
-
                 # Should follow the Kubernetes naming rules
                 pod_name="new-pod-name",
-
                 nodes_per_block=1,
                 init_blocks=1,
                 # Maximum number of pods to scale up
@@ -46,25 +40,31 @@ config = Config(
     ]
 )
 
+
 # App that generates a random number
 @python_app
 def generate(limit):
-      from random import randint
-      return randint(1,limit)
+    from random import randint
+
+    return randint(1, limit)
+
 
 # App that writes a variable to a file
 @bash_app
 def save(variable, outputs=[]):
-      return 'echo %s &> %s' % (variable, outputs[0])
+    return "echo %s &> %s" % (variable, outputs[0])
+
 
 with parsl.load(config):
     # Generate a random number between 1 and 10
     random = generate(10)
-    print('Random number: %s' % random.result())
+    print("Random number: %s" % random.result())
 
     # Save the random number to a file
-    saved = save(random, outputs=[File(os.path.join(os.getcwd(), 'sequential-output.txt'))])
+    saved = save(
+        random, outputs=[File(os.path.join(os.getcwd(), "sequential-output.txt"))]
+    )
 
     # Print the output file
-    with open(saved.outputs[0].result(), 'r') as f:
-          print('File contents: %s' % f.read())
+    with open(saved.outputs[0].result(), "r") as f:
+        print("File contents: %s" % f.read())
